a47eb3e190bb5141f5903902225176faf2d1922c
Pierre M. <promazelaygue@gmail.com>
2025-08-26 22:24:02 +0200

    Ajout d'une animation de fondu au composant NotePaypal : encapsulation du contenu dans un composant FadeIn pour améliorer l'expérience utilisateur, et réorganisation de la structure HTML pour une meilleure lisibilité.

--- src/components/features/NotePaypal.jsx
+++ src/components/features/NotePaypal.jsx
@@ -1,15 +1,19 @@
+import { FadeIn } from "../ui/FadeIn";
+
 const NotePaypal = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center mt-12">
-      <p className="text-purple-100">Paiement sécurisé via</p>
+    <FadeIn>
+      <div className="flex flex-col items-center justify-center text-center mt-12">
+        <p className="text-purple-100">Paiement sécurisé via</p>
 
-      <div className="flex p-3 my-4 border rounded-lg shadow-lg bg-purple-200/60 backdrop-blur-sm border-white/30">
-        <img src="/assets/img/logo-paypal.png" alt="PayPal" className="h-8" />
+        <div className="flex p-3 my-4 border rounded-lg shadow-lg bg-purple-200/60 backdrop-blur-sm border-white/30">
+          <img src="/assets/img/logo-paypal.png" alt="PayPal" className="h-8" />
+        </div>
+        <p className="w-full text-xs text-purple-50">
+          2€ de frais de gestion par Paypal peuvent s'appliquer lors du paiement
+        </p>
       </div>
-      <p className="w-full text-xs text-purple-50">
-        2€ de frais de gestion par Paypal peuvent s'appliquer lors du paiement
-      </p>
-    </div>
+    </FadeIn>
   );
 };
 
