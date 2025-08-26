523fc5a16ed17b22685eb721f8081a5317bfd842
Pierre M. <promazelaygue@gmail.com>
2025-08-26 22:24:38 +0200

    Modification du composant Hero : ajustement des marges, mise à jour des couleurs de texte pour une meilleure lisibilité, et amélioration des styles des boutons. Réorganisation de la structure HTML pour optimiser l'affichage des éléments.

--- src/components/features/common/Hero.jsx
+++ src/components/features/common/Hero.jsx
@@ -2,31 +2,31 @@ import { FadeIn } from "../../ui/FadeIn";
 
 const Hero = () => {
   return (
-    <section id="home" className="relative flex items-center h-full mt-32">
+    <section id="home" className="relative flex items-center h-full mt-28">
       <div className="relative w-full">
         <div className="px-4 mx-auto text-center max-w-5xl sm:px-6 lg:px-8">
           <FadeIn>
-            <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl md:text-5xl lg:text-6xl">
+            <h1 className="text-3xl font-extrabold tracking-tight text-purple-50 sm:text-4xl md:text-5xl lg:text-6xl">
               Découvrez le langage secret de votre Inconscient
             </h1>
           </FadeIn>
 
           <FadeIn className="mt-4 md:mt-6">
-            <p className="text-base sm:text-lg md:text-xl text-slate-200/90">
+            <p className="text-base sm:text-lg md:text-xl text-purple-100">
               Laisser les cartes vous guider pour éclairer votre chemin de vie
             </p>
           </FadeIn>
 
           <FadeIn className="mt-8 md:mt-10">
             <div className="flex flex-col items-stretch justify-center gap-3 sm:flex-row sm:gap-4 sm:items-center">
-              <button className="relative inline-flex items-center justify-center px-6 py-3 w-full text-base font-medium text-white rounded shadow-lg transition-all duration-300 sm:w-auto md:text-lg focus:outline-none bg-gradient-to-r from-purple-500 to-blue-600 shadow-purple-500/25 overflow-hidden group">
-                <div className="absolute inset-0 bg-white transition-transform duration-300 ease-out transform translate-y-full group-hover:translate-y-0"></div>
+              <button className="relative inline-flex items-center justify-center px-6 py-3 w-full text-base font-medium rounded shadow-lg transition-all duration-300 text-purple-50 sm:w-auto md:text-lg focus:outline-none bg-gradient-to-r from-purple-500 to-blue-600 shadow-purple-500/25 overflow-hidden group">
+                <div className="absolute inset-0 transition-transform duration-300 ease-out transform bg-purple-50 translate-y-full group-hover:translate-y-0"></div>
                 <span className="relative z-10 transition-colors duration-300 group-hover:text-purple-900">
                   Réserver un tirage
                 </span>
               </button>
               <button className="relative inline-flex items-center justify-center px-6 py-3 w-full text-base font-medium border rounded transition-all duration-300 sm:w-auto md:text-lg focus:outline-none border-purple-50 text-purple-50 hover:bg-white hover:text-purple-900 hover:border-purple-300 overflow-hidden group">
-                <div className="absolute inset-0 bg-white transition-transform duration-300 ease-out transform translate-y-full group-hover:translate-y-0"></div>
+                <div className="absolute inset-0 transition-transform duration-300 ease-out transform bg-purple-50 translate-y-full group-hover:translate-y-0"></div>
                 <span className="relative z-10 transition-colors duration-300 group-hover:text-purple-900">
                   Découvrir les tirages gratuits sur YouTube
                 </span>
@@ -34,13 +34,10 @@ const Hero = () => {
             </div>
           </FadeIn>
 
-          <FadeIn className="mt-10 md:mt-14">
-            <a
-              href="#about"
-              aria-label="Aller à la section suivante"
-              className="inline-flex items-center justify-center pt-12 mx-auto w-12 h-12 transition-colors duration-300">
+          <FadeIn className="mt-10 lg:mt-20">
+            <div className="inline-flex items-center justify-center mx-auto transition-colors duration-300 scale-75 -mt-12 lg:scale-100">
               <iframe src="https://lottie.host/embed/bb7634e2-3692-4d30-8002-9dc962154b2c/W9fHZGS0aA.lottie"></iframe>
-            </a>
+            </div>
           </FadeIn>
         </div>
       </div>
