703661e2f7b166b29dc191f0c39602c5f97b7604
Pierre M. <promazelaygue@gmail.com>
2025-08-27 13:16:19 +0200

    Modification des styles CSS dans le composant NotePaypal : changement de la couleur du texte et ajustement de l'arrière-plan pour une meilleure lisibilité et esthétique.

--- src/components/features/NotePaypal.jsx
+++ src/components/features/NotePaypal.jsx
@@ -6,9 +6,9 @@ const NotePaypal = () => {
   return (
     <FadeIn>
       <div className="flex flex-col items-center justify-center text-center mt-12">
-        <p className="text-slate-50">Paiement sécurisé via</p>
+        <p className="text-white">Paiement sécurisé via</p>
 
-        <div className="flex p-3 my-4 border rounded-lg shadow-lg bg-slate-50 backdrop-blur-sm border-white/30">
+        <div className="flex p-3 my-4 border rounded-lg shadow-lg bg-white-50 backdrop-blur-sm border-white/30">
           <OptimizedImage
             src={LogoPaypal}
             alt="PayPal"
@@ -16,7 +16,7 @@ const NotePaypal = () => {
             loading="eager"
           />
         </div>
-        <p className="w-full text-xs text-slate-50">
+        <p className="w-full text-xs text-white">
           2€ de frais de gestion par Paypal peuvent s'appliquer lors du paiement
         </p>
       </div>
