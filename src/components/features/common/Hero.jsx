0f0c1b36459fed7a8247fe5f2cf25d3e4cf7116d
Pierre M. <promazelaygue@gmail.com>
2025-08-30 00:09:27 +0200

    Modification des classes CSS dans le composant Hero pour ajuster l'espacement des éléments et améliorer la présentation du texte, en particulier pour les marges des animations FadeIn.

--- src/components/features/common/Hero.jsx
+++ src/components/features/common/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="relative flex items-center py-16 w-full h-screen mt-8">
+      className="relative flex items-center py-16 w-full h-screen lg:mt-8">
       <div className="relative w-full">
         <div className="px-4 mx-auto w-full text-center sm:px-6 lg:px-8">
           <FadeIn>
@@ -13,13 +13,13 @@ const Hero = () => {
             </h1>
           </FadeIn>
 
-          <FadeIn className="mt-8 lg:mt-16">
+          <FadeIn className="mt-4 lg:mt-8">
             <p className="text-base sm:text-lg md:text-xl text-purple-100">
               Laisser les cartes vous guider pour éclairer votre chemin de vie
             </p>
           </FadeIn>
 
-          <FadeIn className="mt-8 md:mt-16">
+          <FadeIn className="mt-12 lg:mt-16">
             <div className="flex flex-col items-center gap-4 sm:flex-row sm:justify-center">
               <a
                 href="#price"
