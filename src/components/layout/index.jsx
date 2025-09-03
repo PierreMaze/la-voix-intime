79546497fb169554f74175a65ee94f0528e731ef
Pierre M. <promazelaygue@gmail.com>
2025-09-03 15:49:04 +0200

    Commentaire de code pour désactiver temporairement le composant InfoBanner dans le layout, sans suppression du code.

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import InfoBanner from "../features/InfoBanner";
+// import InfoBanner from "../features/InfoBanner";
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <InfoBanner />
+      {/* <InfoBanner /> */}
       <main className="pt-12 flex-grow">
         <Outlet />
       </main>
